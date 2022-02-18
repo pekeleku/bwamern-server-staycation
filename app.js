b6b55552af42bea030c21021f553b31a7f00ab4a
pekeleku <alumanwalter@gmail.com>
2022-02-18 10:18:05 +0800

    connect to mongoose

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ var express = require("express");
 var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
+// import mongoose
+mongoose.connect("mongodb://localhost:27017/myapp");
 
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
