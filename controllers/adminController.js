cf48638468e238230f09fa7c317161bc9d1d65e4
pekeleku <alumanwalter@gmail.com>
2022-02-24 20:54:06 +0800

    view dashboard

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -61,10 +61,14 @@ module.exports = {
   viewDashboard: async (req, res) => {
     try {
       const member = await Member.find();
+      const booking = await Booking.find();
+      const item = await Item.find();
       res.render("admin/dashboard/view_dashboard", {
         title: "Staycation | Dashboard",
         user: req.session.user,
         member,
+        booking,
+        item,
       });
     } catch (error) {}
   },
