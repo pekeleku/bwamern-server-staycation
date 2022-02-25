8f44465ea578ba7cbc9ea5a0d9f266a4cc0735ac
pekeleku <alumanwalter@gmail.com>
2022-02-25 20:47:15 +0800

    API Detail Page Project Staycation

--- controllers/apiController.js
+++ controllers/apiController.js
@@ -2,6 +2,7 @@ const Item = require("../models/Item");
 const Treasure = require("../models/Activity");
 const Traveler = require("../models/Booking");
 const Category = require("../models/Category");
+const Bank = require("../models/Bank");
 
 module.exports = {
   landingPage: async (req, res) => {
@@ -40,7 +41,16 @@ module.exports = {
           }
         }
       }
-
+      const testimonial = {
+        _id: "asd1293uasdads1",
+        imageUrl: "/images/testimonial2.jpg",
+        name: "Happy Family",
+        rate: 4.55,
+        content:
+          "What a great trip with my family and I should try again next time soon ...",
+        familyName: "Angga",
+        familyOccupation: "Product Designer",
+      };
       res.status(200).json({
         hero: {
           travelers: traveler.length,
@@ -49,10 +59,32 @@ module.exports = {
         },
         mostPicked,
         category,
+        testimonial,
       });
     } catch (error) {
       console.log(error);
       res.status(500).json({ message: "Internal Server Error" });
     }
+  },
+  detailPage: async (req, res) => {
+    try {
+      const { id } = req.params;
+      const item = await Item.findOne({ _id: id })
+        .populate({ path: "featureId", select: "_id name qty imageUrl" })
+        .populate({ path: "activityId", select: "_id name type imageUrl" })
+        .populate({ path: "imageId", select: "_id imageUrl" });
+      const bank = await Bank.find();
+      const testimonial = {
+        _id: "asd1293uasdads1",
+        imageUrl: "/images/testimonial1.jpg",
+        name: "Happy Family",
+        rate: 4.55,
+        content:
+          "What a great trip with my family and I should try again next time soon ...",
+        familyName: "Angga",
+        familyOccupation: "Product Designer",
+      };
+      res.status(200).json({ ...item._doc, bank, testimonial });
+    } catch (error) {}
   },
 };
--- routes/api.js
+++ routes/api.js
@@ -3,5 +3,6 @@ const apiController = require("../controllers/apiController");
 // const { upload, uploadMultiple } = require("../middlewares/multer");
 
 router.get("/landing-page", apiController.landingPage);
+router.get("/detail-page/:id", apiController.detailPage);
 
 module.exports = router;
